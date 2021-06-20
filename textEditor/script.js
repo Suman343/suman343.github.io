0da6855856ce41de1816a203364f6c70849772bb
Suman343 <70128159+Suman343@users.noreply.github.com>
2021-06-20 20:20:33 +0545

    texEditor final-commit
    Text Editor web app added.

---
+++ textEditor/script.js
@@ -0,0 +1,43 @@
+
+// to copy the user inputed text to output text area.. 
+function updateText() {
+  let text = document.getElementById("text-input").value;
+  document.getElementById("text-output").innerText = text;
+}
+
+
+function alignText(elem, alignType) {
+  document.getElementById("text-output").style.textAlign = alignType;
+  let buttonsList = document.getElementsByClassName("align");
+  for (let i = 0; i < buttonsList.length; i++) {
+    buttonsList[i].classList.remove("active");
+  }
+  elem.classList.add("active");
+}
+
+
+
+function makeBold(elem) {
+  elem.classList.toggle("active");
+  document.getElementById("text-output").classList.toggle("bold");
+}
+
+
+function makeItalic(elem) {
+  elem.classList.toggle("active");
+  document.getElementById("text-output").classList.toggle("italic");
+}
+
+
+function makeUnderline(elem) {
+  elem.classList.toggle("active");
+  let formattedText = document.getElementById("text-output");
+  if (formattedText.classList.contains("underline")) {
+    formattedText.classList.remove("underline");
+  } else {
+    formattedText.classList.add("underline");
+  }
+}
+
+
+
